bb261d6632941ec3dcef3969e256b1fa6281a07f
Anthony Frank <afrank84@yahoo.com>
2024-11-20 08:11:21 -0500

    VisualStudio2019-DownloadWithOutEmail.ps1

---
+++ Install/VisualStudio2019-DownloadWithOutEmail.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest -Uri "https://aka.ms/vs/16/release/vs_community.exe" -OutFile "C:/VS2019/vs_community.exe"
+Invoke-WebRequest -Uri "https://aka.ms/vs/16/release/vs_professional.exe" -OutFile "C:/VS2019/vs_professional.exe"
+Invoke-WebRequest -Uri "https://aka.ms/vs/16/release/vs_enterprise.exe" -OutFile "C:/VS2019/vs_enterprise.exe"
