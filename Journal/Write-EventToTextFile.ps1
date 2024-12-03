574006a3628a771fba76a638b764e5801cb16ba0
Anthony Frank <afrank84@yahoo.com>
2024-12-03 13:47:38 -0500

    Write-EventToTextFile.ps1 - author name now generic

--- Journal/Write-EventToTextFile.ps1
+++ Journal/Write-EventToTextFile.ps1
@@ -2,6 +2,6 @@
 ## Asks users a single question and stores response in Text Document
 $UserInput = Read-Host -Prompt 'Describe what just happened'
 $eventTimeStamp = Get-Date
-$author = 'Anthony Frank'
+$author = 'Author_Name'
 Add-Content -path "../Data/event_journal.txt"  "$eventTimeStamp | $author : $UserInput"
 Write-Output "Thank you for your Input"
