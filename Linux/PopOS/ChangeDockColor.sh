f9561ae31ae99f74f8d68828e10f2a7908577476
Anthony Frank <afrank84@yahoo.com>
2025-04-22 10:15:58 -0400

    ChangeDockColor.sh - the default is what I use for work

---
+++ Linux/PopOS/ChangeDockColor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set dock background color to Oxford Blue
+gsettings set org.gnome.shell.extensions.dash-to-dock background-color '#002147'
+
+# Set dock background opacity
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.8
+
+# Optional: Print confirmation
+echo "Dock color set to Oxford Blue (#002147) with 80% opacity."
