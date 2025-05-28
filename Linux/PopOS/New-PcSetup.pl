63cc73bd306fa670fda1a894028261b2bdfbd569
Anthony Frank <afrank84@yahoo.com>
2025-05-28 14:26:49 -0400

    New-PcSetup.pl - added exit, obsidian, and git

--- Linux/PopOS/New-PcSetup.pl
+++ Linux/PopOS/New-PcSetup.pl
@@ -2,18 +2,21 @@
 use strict;
 use warnings;
 
+# Function to execute system commands and handle errors
 sub run_cmd {
     my ($cmd) = @_;
-    print "Running: $cmd\n";
+    print "Executing: $cmd\n";
     system($cmd) == 0 or die "Command failed: $cmd\n";
 }
 
+# Function to check if a package is installed
 sub is_installed {
     my ($pkg) = @_;
     my $output = `dpkg-query -l | grep $pkg 2>/dev/null`;
     return $output ne '';
 }
 
+# Function to install curl if not already installed
 sub install_curl_if_needed {
     if (system("command -v curl > /dev/null 2>&1") != 0) {
         print "Installing curl...\n";
@@ -24,14 +27,14 @@ sub install_curl_if_needed {
     }
 }
 
+# Function to install Brave Browser
 sub install_brave {
-    print "Checking if Brave Browser is already installed...\n";
+    print "\n--- Installing Brave Browser ---\n";
     if (is_installed("brave-browser")) {
         print "Brave Browser is already installed.\n";
         return;
     }
 
-    print "Installing Brave Browser...\n";
     install_curl_if_needed();
 
     print "Adding Brave's GPG key...\n";
@@ -50,14 +53,14 @@ sub install_brave {
     print "Brave Browser installation completed.\n";
 }
 
+# Function to install Visual Studio Code
 sub install_vscode {
-    print "Checking if Visual Studio Code is already installed...\n";
+    print "\n--- Installing Visual Studio Code ---\n";
     if (is_installed("code")) {
         print "Visual Studio Code is already installed.\n";
         return;
     }
 
-    print "Installing Visual Studio Code...\n";
     install_curl_if_needed();
 
     print "Adding Microsoft's GPG key...\n";
@@ -76,20 +79,72 @@ sub install_vscode {
     print "Visual Studio Code installation completed.\n";
 }
 
-# Main menu
-print "\nChoose an option:\n";
-print "1. Install Brave Browser\n";
-print "2. Install Visual Studio Code\n";
-print "3. Install Both\n";
-print "Enter your choice (1/2/3): ";
+# Function to install Obsidian
+sub install_obsidian {
+    print "\n--- Installing Obsidian ---\n";
+    if (is_installed("obsidian")) {
+        print "Obsidian is already installed.\n";
+        return;
+    }
 
-chomp(my $choice = <STDIN>);
+    # Check if snap is installed
+    if (system("command -v snap > /dev/null 2>&1") != 0) {
+        print "Installing snapd...\n";
+        run_cmd("sudo apt update");
+        run_cmd("sudo apt install -y snapd");
+    } else {
+        print "snapd is already installed.\n";
+    }
 
-if    ($choice eq '1') { install_brave(); }
-elsif ($choice eq '2') { install_vscode(); }
-elsif ($choice eq '3') {
-    install_brave();
-    install_vscode();
-} else {
-    print "Invalid choice. Exiting.\n";
+    print "Installing Obsidian via snap...\n";
+    run_cmd("sudo snap install obsidian --classic");
+    print "Obsidian installation completed.\n";
 }
+
+# Function to install Git
+sub install_git {
+    print "\n--- Installing Git ---\n";
+    if (is_installed("git")) {
+        print "Git is already installed.\n";
+        return;
+    }
+
+    run_cmd("sudo apt update");
+    run_cmd("sudo apt install -y git");
+    print "Git installation completed.\n";
+}
+
+# Main menu
+sub main_menu {
+    print "\nSelect an option to install:\n";
+    print "1. Brave Browser\n";
+    print "2. Visual Studio Code\n";
+    print "3. Obsidian\n";
+    print "4. Git\n";
+    print "5. Install All\n";
+    print "6. Exit\n";
+    print "Enter your choice (1-6): ";
+
+    chomp(my $choice = <STDIN>);
+
+    if    ($choice eq '1') { install_brave(); }
+    elsif ($choice eq '2') { install_vscode(); }
+    elsif ($choice eq '3') { install_obsidian(); }
+    elsif ($choice eq '4') { install_git(); }
+    elsif ($choice eq '5') {
+        install_brave();
+        install_vscode();
+        install_obsidian();
+        install_git();
+    }
+    elsif ($choice eq '6') {
+        print "Exiting the installer. Goodbye!\n";
+        exit 0;
+    }
+    else {
+        print "Invalid choice. Please enter a number between 1 and 6.\n";
+    }
+}
+
+# Run the main menu
+main_menu();
